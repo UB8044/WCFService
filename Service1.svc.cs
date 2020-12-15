a324fe117dd7692d84ed68fb18c93a161be99c44
Fakhrizar Rifqi Fuad <fakhrizar.rifqi.ft18@mail.umy.ac.id>
2020-12-15 22:14:27 +0700

    First Commit

--- Service1.svc.cs
+++ Service1.svc.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
@@ -12,22 +14,78 @@ namespace WcfService1
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
     {
-        public string GetData(int value)
+        public string Insert(InsertUser user)
         {
-            return string.Format("You entered: {0}", value);
+            string msg;
+            SqlConnection con = new SqlConnection("Data Source = LAPTOP-N38C71DV; Initial Catalog = MyTestDB; Persist Security Info=True; User ID=sa; Password=Kawan123; Pooling=False");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Insert into Usertab (Nama, Email) values (@Name, @Email)", con);
+            cmd.Parameters.AddWithValue("@Name", user.Name);
+            cmd.Parameters.AddWithValue("@Email", user.Email);
+
+            int g = cmd.ExecuteNonQuery();
+            if (g == 1)
+            {
+                msg = "Successfully Inserted";
+            }
+            else
+            {
+                msg = "Failed to insert";
+            }
+            return msg;
         }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        public gettestdata GetInfo()
         {
-            if (composite == null)
+            gettestdata g = new gettestdata();
+            SqlConnection con = new SqlConnection("Data Source = LAPTOP-N38C71DV; Initial Catalog = MyTestDB; Persist Security Info=True; User ID=sa; Password=Kawan123; Pooling=False");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from UserTab", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable("mytab");
+            da.Fill(dt);
+            g.usertab = dt;
+            return g;
+        }
+
+        public string Update(UpdateUser u)
+        {
+            string Message = "";
+            SqlConnection con = new SqlConnection("Data Source = LAPTOP-N38C71DV; Initial Catalog = MyTestDB; Persist Security Info=True; User ID=sa; Password=Kawan123; Pooling=False");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("update UserTab set Nama = @Name, Email = @Email where UserID = @UserID", con);
+            cmd.Parameters.AddWithValue("@UserID", u.UID);
+            cmd.Parameters.AddWithValue("@Name", u.Name);
+            cmd.Parameters.AddWithValue("@Email", u.Email);
+            int res = cmd.ExecuteNonQuery();
+            if (res == 1)
             {
-                throw new ArgumentNullException("composite");
+                Message = "Successfull Updated";
             }
-            if (composite.BoolValue)
+            else
             {
-                composite.StringValue += "Suffix";
+                Message = "Failed to Update";
             }
-            return composite;
+            return Message;
+        }
+
+        public string Delete(DeleteUser d)
+        {
+            string msg = "";
+            SqlConnection con = new SqlConnection("Data Source = LAPTOP-N38C71DV; Initial Catalog = MyTestDB; Persist Security Info=True; User ID=sa; Password=Kawan123; Pooling=False");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("delete UserTab where UserID = @UserID", con);
+            cmd.Parameters.AddWithValue("@UserID", d.UID);
+            int res = cmd.ExecuteNonQuery();
+            if (res == 1)
+            {
+                msg = "Successfull Deleted";
+            }
+            else
+            {
+                msg = "Failed to Delete";
+            }
+            return msg;
         }
     }
 }
